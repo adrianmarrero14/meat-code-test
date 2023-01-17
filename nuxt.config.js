e91a0337bbfd080c4c7d00374b413536a1b5e515
Adrian <adrianmarrero18@gmail.com>
2023-01-17 13:38:16 -0400

    nuxt.config and axios installed

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    "~/plugins/axios.js",
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -38,13 +39,14 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    "@nuxtjs/axios",
   ],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
